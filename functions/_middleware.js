162bd21f45b41f3da5b610693669430a7a466992
hamuzon <nagasawa.1399@gmail.com>
2025-12-07 14:17:39 +0900

    _middleware.js office_Version_1.0.0_Release_2.0

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -7,18 +7,31 @@ export async function onRequest(context) {
 
   const ua = request.headers.get("user-agent") || "";
   const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(ua);
-// hamusata.f5.si 系列以外はそのまま
+
+  // hamusata.f5.si 系列以外はそのまま
   if (!hostname.endsWith("hamusata.f5.si")) {
     return context.next();
   }
 
   // Mobile redirect
+  // モバイル端末の場合、m. が付いていなければ付加してリダイレクト
   if (isMobile && !hostname.startsWith("m.")) {
-    url.hostname = hostname.replace(/^www\./, '').replace(/^/, hostname.startsWith('www.') ? 'www.m.' : 'm.');
+
+    // www.m. はつけない
+    if (hostname.startsWith("www.")) {
+      // www + PC → www.m.
+      const base = hostname.replace(/^www\./, ""); // hamusata.f5.si
+      url.hostname = "www.m." + base;
+    } else {
+      // PC → m.
+      url.hostname = "m." + hostname;
+    }
+
     return Response.redirect(url.toString(), 302);
   }
 
   // PC redirect
+  // PC端末の場合、m. が付いていれば削除してリダイレクト
   if (!isMobile && hostname.startsWith("m.")) {
     url.hostname = hostname.replace(/^m\./, '');
     return Response.redirect(url.toString(), 302);
