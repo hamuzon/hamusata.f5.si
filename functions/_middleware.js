0c5a892aba46996071e37c17efc75c8a38a0ab29
hamuzon <nagasawa.1399@gmail.com>
2025-12-07 13:56:45 +0900

    _middleware.js v1.0.0 Release1

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -8,23 +8,23 @@ export async function onRequest(context) {
   const ua = request.headers.get("user-agent") || "";
   const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(ua);
 
-  // 対象ドメイン以外はそのまま
+
   if (!hostname.endsWith("hamusata.f5.si")) {
     return context.next();
   }
 
-  // モバイルは m.hamusata.f5.si へ
+  // mobile: m.hamusata.f5.si 
   if (isMobile && hostname !== "m.hamusata.f5.si") {
     url.hostname = "m.hamusata.f5.si";
     return Response.redirect(url.toString(), 302);
   }
 
-  // PCは hamusata.f5.si へ
+  // PC : hamusata.f5.si 
   if (!isMobile && hostname !== "hamusata.f5.si") {
     url.hostname = "hamusata.f5.si";
     return Response.redirect(url.toString(), 302);
   }
 
-  // 正しいサブドメインならそのまま表示
+  // 
   return context.next();
 }
