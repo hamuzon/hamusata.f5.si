6d4c0e521fb9ef72f4577a22d2fcc735548595ad
hamuzon <github@hamusata.f5.si>
2026-09-10 12:03:30 +0900

    feat: initialize project with package.json for Electron app setup

---
+++ electron/main.cjs
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadURL("https://hamusata.f5.si/");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
